a7da38f3306f1ac08181dee6025a2b23b05d6800
jbginer <pro.giner@outlook.fr>
2022-04-22 18:08:34 +0200

    Gros nettoyage des ressources annexes

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    #region Variables
+    public int damage;
+    #endregion
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerStats = other.GetComponent<PlayerStats>();
+
+        if (playerStats != null)
+        {
+            playerStats.TakeDamage(damage);
+        }
+    }
+}
